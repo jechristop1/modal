6f6a80e83839bb6ee1edf0d79eec3e28d22c62ae
Joshua Christopherson <jechristop1@gmail.com>
2015-07-07 13:10:50 -0700

    changed to classList Toggle

--- js/script.js
+++ js/script.js
@@ -2,11 +2,7 @@ var open = document.getElementById('openModal');
 var overlay = document.getElementById('overlay');
 var close = document.getElementById('closeModal');
 function showOrHide(){
-	if(overlay.classList.contains('hide')){
-		overlay.classList.remove('hide');
-	}else{
-		overlay.classList.add('hide');
-	}
+	overlay.classList.toggle('hide');
 };
 open.addEventListener('click', showOrHide);
 close.addEventListener('click', showOrHide);
